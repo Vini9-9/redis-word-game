62a95b6296e6c84ce9949b0af18482d2249c01f2
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2021-12-14 22:01:07 -0300

    Fazendo um dicionário

--- test/5.2.js
+++ test/5.2.js
@@ -6,7 +6,8 @@ var redis = require('./redis');
 var path = require('path');
 
 var DICTIONARY_KEY = 'dictionary';
-var dictionary_file = path.resolve(__dirname, '5.2.dictionary');
+//var dictionary_file = path.resolve(__dirname, '5.2.dictionary');
+var dictionary_file = path.resolve(__dirname, '5.2.dic');
 
 describe('5.2', function() {
 
@@ -16,7 +17,7 @@ describe('5.2', function() {
 
   describe('dictionary', function() {
 
-    it('should import a dictionary file', function(done) {
+    it('should import a dictionary file | Deve importar um arquivo dicionário', function(done) {
       dictionary.importFile(dictionary_file, function(err, result) {
         should.not.exist(err);
         result.should.eql(10);
@@ -26,21 +27,23 @@ describe('5.2', function() {
           should.exist(members);
           members.should.be.an.array;
           members.length.should.eql(10);
-          members.should.containEql('Scottie', 'Scottish');
+          //members.should.containEql('Scottie', 'Scottish');
+          members.should.containEql('titã', 'tiro');
           done();
         });
       });
     });
 
-    it('should validate a good word', function(done) {
-      dictionary.isValidWord('Scott', function(err, result) {
+    it('should validate a good word | Deve validar uma palavra correta', function(done) {
+      //dictionary.isValidWord('Scott', function(err, result) {
+        dictionary.isValidWord('titi', function(err, result) {
         should.not.exist(err);
         result.should.be.true;
         done();
       });
     });
 
-    it('should not validate a bad word', function(done) {
+    it('should not validate a bad word | Não deve validar uma palavra incorreta', function(done) {
       dictionary.isValidWord('NOT_A_WORD', function(err, result) {
         should.not.exist(err);
         result.should.be.false;
